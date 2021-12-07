e9382eed3aaa88c8f8bf8e8756854446466a5d7f
Artyomus1337 <92204916+Artyomus1337@users.noreply.github.com>
2021-12-07 23:06:10 +0100

    Update Config.cs

--- AntiNorthwood/Config.cs
+++ AntiNorthwood/Config.cs
@@ -10,6 +10,6 @@ namespace AntiNorthwood
         [Description("Whether the plugin is enabled or not.")]
         public bool IsEnabled { get; set; } = true;
         [Description("Message shown to nortwood member if he's gonna get kicked from server")]
-        public string message { get; set; } = "ddasds";
+        public string message { get; set; } = "gay";
     }
 }
